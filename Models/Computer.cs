2989ed928c776626a91c395e756188ffd59f0a08
Kevin Haggerty <kghaggerty@gmail.com>
2018-01-24 15:03:16 -0600

    changed file names to singular posession

--- Models/Computer.cs
+++ Models/Computer.cs
@@ -15,6 +15,6 @@ namespace BangazonAPI.Models
 
     public string DecommissionDate { get; set; }
 
-    public ICollection<EmployeeComputer> EmployeesComputer;
+    public ICollection<EmployeeComputer> EmployeeComputer;
   }
 }
